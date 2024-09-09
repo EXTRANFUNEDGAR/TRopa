7f976cf40355e077da733c7893ac22ae2614c3c0
Luis01-AE <157388343+LuisA932-lgm@users.noreply.github.com>
2024-09-08 19:49:15 -0600

    Add files via upload

---
+++ VistaConfiguración.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <script src="assets/js/scripts.js" defer></script>
+    <title>Encabezado con Notificaciones</title>
+</head>
+<body>
+    <header>
+        <button class="open-btn" onclick="openSidebar()">☰</button>
+
+        <form method="get" target="">
+            <input class="buscar" type="search" name="q" placeholder="Buscar">
+            <button type="submit" class="btnbuscar">
+                <img src="assets/img/busqueda.svg" alt="Busqueda" style="filter: invert(1);">
+            </button>
+        </form>
+
+        <button class="carrito">
+            <img src="assets/img/carrito.svg" alt="Carrito" style="filter: invert(1);">
+        </button>
+
+        <button class="open-btn" onclick="openSidebar1()">
+            <img src="assets/img/usuario.svg" alt="Usuario" style="filter: invert(1);">
+        </button>
+    </header>
+
+    <div id="sidebar" class="sidebar left-sidebar">
+        <a href="javascript:void(0)" class="close-btn" onclick="closeSidebar()">&times;</a>
+        <a href="#inicio">Categoría 1</a>
+        <a href="#servicios">Categoría 2</a>
+        <a href="#nosotros">Categoría 3</a>
+        <a href="#contacto">Categoría 4</a>
+    </div>
+
+    <div id="sidebar1" class="sidebar right-sidebar">
+        <a href="javascript:void(0)" class="close-btn" onclick="closeSidebar1()">&times;</a>
+        <center>
+            <a href="javascript:void(0)" onclick="toggleNotifications()">Notificaciones</a>
+            <a href="#">Mis Compras</a>
+            <a href="#">Configuración</a>
+            <a href="#">Cerrar Sesión</a>
+        </center>
+
+        <div id="notifications" class="notifications">
+            <div class="notification-content">
+                <p>Notificación 1</p>
+                <p>Notificación 2</p>
+                <p>Notificación 3</p>
+            </div>
+        </div>
+    </div>
+    <div class="barra1">
+      <br>
+      <b>Cuenta</b>
+      <br>
+      <p style="color: gray;"> Personalización de la cuenta</p>
+      
+    <hr> 
+<b>Región</b><br><p style="color: gray;">Cambiar idioma o región</p><hr>
+<b>Envío</b><br><p style="color: gray;">Preferencias de envío, direcciones guardadas</p><hr>
+<b>Historial</b><br><p style="color: gray;">Borrar historial de búsqueda</p><hr>
+<b>Acerca del sitio web</b><br>
+       
+    </div>
+</body>
+</html>
