2cf99982499934d938f87b21f594660e3a5ce52e
Luis01-AE <157388343+LuisA932-lgm@users.noreply.github.com>
2024-10-24 20:48:14 -0600

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,3 +25,26 @@ function closeSidebar1() {
     sidebar1.style.width = "0";
     document.getElementById("notifications").style.display = "none"; // Cierra notificaciones al cerrar sidebar
 }
+// assets/js/scripts.js
+
+function previewImage(event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById('imagePreview');
+
+    // Clear previous preview
+    preview.innerHTML = '';
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            preview.appendChild(img);
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        preview.innerHTML = '<p>No se seleccionó ninguna imagen.</p>';
+    }
+}
